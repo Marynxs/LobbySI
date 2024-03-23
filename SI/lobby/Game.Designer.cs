03b9d3c129c50075d170dd07b6678be101e08ede
Marynxs <122127061+Marynxs@users.noreply.github.com>
2024-03-23 18:12:38 -0300

    Add files via upload

---
+++ SI/lobby/Game.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace lobby
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstCartas = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lstCartas
+            // 
+            this.lstCartas.FormattingEnabled = true;
+            this.lstCartas.Location = new System.Drawing.Point(28, 120);
+            this.lstCartas.Name = "lstCartas";
+            this.lstCartas.Size = new System.Drawing.Size(232, 303);
+            this.lstCartas.TabIndex = 0;
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstCartas);
+            this.Name = "Game";
+            this.Text = "Game";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstCartas;
+    }
+}
---
+++ SI/lobby/Lobby.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MagicTrickServer;
+
+namespace lobby
+{
+    public partial class Lobby : Form
+    {
+        //variaveis globais
+        List<string> dadosJogador = new List<string>();
+
+
+        public Lobby()
+        {
+            InitializeComponent();
+            lblVersao.Text = Jogo.Versao;
+        }
+
+        private void btnPartidas_Click(object sender, EventArgs e)
+        {
+            string partidasList = Jogo.ListarPartidas("T");
+            partidasList = partidasList.Replace("\r", "");
+            if (partidasList.Length > 0)
+                partidasList = partidasList.Substring(0, partidasList.Length - 1);
+
+            string[] nomePartidas = partidasList.Split('\n');
+
+            lstPartsEncontradas.Items.Clear();
+
+            
+            for (int i = 0; i < nomePartidas.Length; i++)
+            {
+                lstPartsEncontradas.Items.Add(nomePartidas[i]);
+
+            }
+
+        }
+
+
+        private void btnListJogadores_Click(object sender, EventArgs e)
+        {
+
+            if (lstPartsEncontradas.SelectedItem != null) // Verifica se há uma partida selecionada na primeira ListBox
+            {
+                string partidaList = lstPartsEncontradas.SelectedItem.ToString();
+                string[] partidInfo = partidaList.Split(',');
+
+                int idPartida = Convert.ToInt32(partidInfo[0]);
+
+                string jogadoresList = Jogo.ListarJogadores(idPartida);
+                lstJogadores.Items.Clear();
+
+                jogadoresList = jogadoresList.Replace("\r", "");
+                if (jogadoresList.Length > 0)
+                    jogadoresList = jogadoresList.Substring(0, jogadoresList.Length - 1);
+
+                string[] nomeJogadores = jogadoresList.Split('\n');
+
+                for (int i = 0; i < nomeJogadores.Length; i++)
+                {
+                    lstJogadores.Items.Add(nomeJogadores[i]);
+                }
+            }
+            else
+            {
+                MessageBox.Show("selecione uma partida antes de listar os jogadores" , "MagicTrick" , MessageBoxButtons.OK , MessageBoxIcon.Exclamation);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void btnCriarPartida_Click(object sender, EventArgs e)
+        {
+            string nomePartida = txtNomePartida.Text;
+            string senha = txtSenha.Text;
+            string grupo = "Helsinque";
+
+            
+
+            string retornoCriacao =  Jogo.CriarPartida(nomePartida, senha, grupo);
+
+             
+            if (retornoCriacao.Length > 4 && retornoCriacao.Substring(0,4) == "ERRO")
+            {
+                MessageBox.Show($"Ocorreu um erro ao criar a partida:\n{retornoCriacao.Substring(5)}", "MagicTrick", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            txtID.Text = retornoCriacao;
+
+
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            if (txtPlayerName.Text != "" && txtSenhaLogIn.Text != "" && txtID.Text != "")
+            {
+                string nomeDoJogador = txtPlayerName.Text;
+                string senhaEntrar = txtSenhaLogIn.Text;
+                int idPartida = Convert.ToInt32(txtID.Text);
+                string retornoEntrar = Jogo.EntrarPartida(idPartida, nomeDoJogador, senhaEntrar) + ',' + nomeDoJogador;
+                if (retornoEntrar.Length > 4 && retornoEntrar.Substring(0, 4) == "ERRO")
+                {
+                    MessageBox.Show($"Ocorreu um erro ao entrar a partida:\n{retornoEntrar.Substring(5)}", "MagicTrick", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                dadosJogador.Add(retornoEntrar);
+                btnListJogadores_Click(sender, e);
+            }
+            else
+            {
+                MessageBox.Show($"Ocorreu um erro ao entrar na partida:\nUm ou mais campos estão vazios", "MagicTrick", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private void btnStartMatch_Click(object sender, EventArgs e)
+        {
+            string[] dados = dadosJogador.ToArray();
+            //Ve na lista de jogadores e separa cada informação de cada jogador em uma matriz [jogador][informação] ex. pega id do primeiro jogador: [0][0]
+            string[][] dadosJogadores = new string[dados.Length][];
+            for (int i = 0; i < dados.Length; i++)
+            {
+                string[] atual = dados[i].Split(',');
+                dadosJogadores[i] = new string[atual.Length];
+                for (int j = 0; j < atual.Length; j++)
+                {
+                    dadosJogadores[i][j] = atual[j];
+                }
+
+            }
+
+            Random random = new Random();
+
+            int randomJogador = random.Next(dados.Length);
+            lblNome.Text = dadosJogadores[randomJogador][2];
+            lblSenha.Text = dadosJogadores[randomJogador][1];
+
+            string retornoIniciar = Jogo.IniciarPartida(Convert.ToInt32(dadosJogadores[randomJogador][0]), dadosJogadores[randomJogador][1]);
+            if (retornoIniciar.Length > 4 && retornoIniciar.Substring(0, 4) == "ERRO")
+            {
+                MessageBox.Show($"Ocorreu um erro ao iniciar a partida:\n{retornoIniciar.Substring(5)}", "MagicTrick", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            lblId.Text = retornoIniciar;
+        }
+
+    }
+}
